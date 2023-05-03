ed90724744a4fba51c35ecd2268477e664c0b92c
Ren-Amamyia <85029153+Ren-Amamyia@users.noreply.github.com>
2023-05-03 21:35:22 +0100

    Update Util and add XML Deserialize method to XmlSerializing class

--- Serializing/XmlSerializing.cs
+++ Serializing/XmlSerializing.cs
@@ -1,5 +1,46 @@
-﻿namespace PTI_API.Serializing; 
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace PTI_API.Serializing; 
 
 public class XmlSerializing {
     
+    public object Deserialize(string filePath, Encoding? encoding) {
+        string[] lines =  File.ReadAllLines(filePath, encoding ?? Encoding.UTF8);
+        Ship s = new Ship();
+        PropertyInfo[] props = s.GetType().GetProperties();
+        PropertyInfo[] dimProps = props[3].GetType().GetProperties();
+        s.Dimensions = new ShipDimensions();
+        char[] separators = new char[] { '<', '>' };
+        foreach (string line in lines) {
+            string[] attrib = line.Split(separators);
+            Console.WriteLine(attrib[2]);
+            switch (attrib[1]) {
+                case "Name":
+                    s.Name = attrib[2];
+                    break;
+                case "Type":
+                    s.Type = Util.NameToTypeDict[attrib[2]];
+                    break;
+                case "Class":
+                    s.Class = attrib[2];
+                    break;
+                case "Length":
+                    s.Dimensions.Length = Int32.Parse(attrib[2]);
+                    break;
+                case "Width":
+                    s.Dimensions.Width = Int32.Parse(attrib[2]);
+                    break;
+                case "Height":
+                    s.Dimensions.Height = Int32.Parse(attrib[2]);
+                    break;
+                default:
+                    continue;
+                
+            }
+        }
+
+        return s;
+    }
 }
--- Util.cs
+++ Util.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 
 public class Util {
     public static Dictionary<ShipType, string> TypeDict = new Dictionary<ShipType, string>();
+    public static Dictionary<string, ShipType> NameToTypeDict = new Dictionary<string, ShipType>();
 
     public static void InitializeDictionary() {
+        // Map ShipType enum values to a string name
         TypeDict.Add(ShipType.Corvette, "Corvette");
         TypeDict.Add(ShipType.Frigate, "Frigate");
         TypeDict.Add(ShipType.Destroyer, "Destroyer");
@@ -13,5 +15,14 @@ public class Util {
         TypeDict.Add(ShipType.Battlecruiser, "Battlecruiser");
         TypeDict.Add(ShipType.Battleship, "Battleship");
         TypeDict.Add(ShipType.Carrier, "Carrier");
+        
+        // Map String names to ShipType enum values
+        NameToTypeDict.Add("Corvette", ShipType.Corvette);
+        NameToTypeDict.Add("Frigate", ShipType.Frigate);
+        NameToTypeDict.Add("Destroyer", ShipType.Destroyer);
+        NameToTypeDict.Add("Cruiser", ShipType.Cruiser);
+        NameToTypeDict.Add("Battlecruiser", ShipType.Battlecruiser);
+        NameToTypeDict.Add("Battleship", ShipType.Battleship);
+        NameToTypeDict.Add("Carrier", ShipType.Carrier);
     }
 }
